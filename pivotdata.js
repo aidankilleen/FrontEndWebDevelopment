f18ba1b2fdc1112523cd6af9aa345ce3f2af3d99
Aidan Killeen <aidan.killeen@gmail.com>
2022-01-16 13:18:33 +0000

    added parameterised pivot function, added summary selection for pivot, fixed bugs with null entries

---
+++ pivotdata.js
@@ -0,0 +1,399 @@
+let data = [
+    {
+      "date": "01/01/2021",
+      "user": "Aidan",
+      "colour": "red",
+      "quantity": 10
+    },
+    {
+      "date": "10/01/2021",
+      "user": "Bob",
+      "make": "Nissan", 
+      "quantity": 12
+    },
+    {
+      "date": "01/11/2021",
+      "user": "Bob",
+      "colour": "red",
+      "make": "Nissan", 
+      "quantity": 14
+    },
+    {
+      "date": "15/08/2021",
+      "user": "Carol",
+      "colour": "green",
+      "make": "Nissan", 
+      "quantity": 15
+    },
+    {
+      "date": "19/01/2021",
+      "user": "Dan",
+      "colour": "red",
+      "make": "Nissan", 
+      "quantity": 16
+    },
+    {
+      "date": "20/03/2021",
+      "user": "Bob",
+      "colour": "red",
+      "make": "Nissan", 
+      "quantity": 20
+    },
+    {
+      "date": "04/05/2021",
+      "user": "Dan",
+      "colour": "green",
+      "make": "Nissan", 
+      "quantity": 1
+    },
+    {
+      "date": "05/04/2021",
+      "user": "Bob",
+      "colour": "red",
+      "make": "Nissan", 
+      "quantity": 20
+    },
+    {
+      "date": "07/09/2021",
+      "user": "Dan",
+      "colour": "red",
+      "make": "Nissan", 
+      "quantity": 9
+    },
+    {
+      "date": "03/02/0201",
+      "user": "Dan",
+      "colour": "green",
+      "make": "Nissan", 
+      "quantity": 14
+    },
+    {
+      "date": "23/08/2021",
+      "user": "Dan",
+      "colour": "red",
+      "make": "Nissan", 
+      "quantity": 23
+    },
+    {
+      "date": "23/12/2021",
+      "user": "Bob",
+      "colour": "red",
+      "make": "Nissan", 
+      "quantity": 21
+    },
+    {
+      "date": "29/12/2021",
+      "user": "Alice",
+      "colour": "green",
+      "make": "Nissan", 
+      "quantity": 20
+    },
+    {
+      "date": "15/03/2021",
+      "user": "Bob",
+      "colour": "orange",
+      "make": "Nissan", 
+      "quantity": 17
+    },
+    {
+      "date": "16/04/2021",
+      "user": "Alice",
+      "colour": "green",
+      "make": "Nissan", 
+      "quantity": 20
+    },
+    {
+      "date": "25/10/2021",
+      "user": "Alice",
+      "colour": "red",
+      "make": "Nissan", 
+      "quantity": 28
+    },
+    {
+      "date": "10/10/2021",
+      "user": "Alice",
+      "colour": "red",
+      "make": "Nissan", 
+      "quantity": 11
+    },
+    {
+      "date": "05/11/2021",
+      "user": "Dan",
+      "colour": "green",
+      "make": "Mercedes", 
+      "quantity": 10
+    },
+    {
+      "date": "09/11/2021",
+      "user": "Carol",
+      "colour": "orange",
+      "make": "Mercedes", 
+      "quantity": 9
+    },
+    {
+      "date": "02/01/2021",
+      "user": "Carol",
+      "colour": "blue",
+      "make": "Mercedes", 
+      "quantity": 10
+    },
+    {
+      "date": "11/01/2021",
+      "user": "Bob",
+      "colour": "orange",
+      "make": "Mercedes", 
+      "quantity": 12
+    },
+    {
+      "date": "02/11/2021",
+      "user": "Bob",
+      "colour": "orange",
+      "make": "Mercedes", 
+      "quantity": 14
+    },
+    {
+      "date": "16/08/2021",
+      "user": "Alice",
+      "colour": "orange",
+      "make": "Mercedes", 
+      "quantity": 15
+    },
+    {
+      "date": "20/01/2021",
+      "user": "Dan",
+      "colour": "blue",
+      "make": "Mercedes", 
+      "quantity": 16
+    },
+    {
+      "date": "21/03/2021",
+      "user": "Carol",
+      "colour": "blue",
+      "make": "Mercedes", 
+      "quantity": 20
+    },
+    {
+      "date": "05/05/2021",
+      "user": "Alice",
+      "colour": "red",
+      "make": "Mercedes", 
+      "quantity": 1
+    },
+    {
+      "date": "06/04/2021",
+      "user": "Carol",
+      "colour": "red",
+      "make": "Mercedes", 
+      "quantity": 20
+    },
+    {
+      "date": "08/09/2021",
+      "user": "Carol",
+      "colour": "red",
+      "make": "Mercedes", 
+      "quantity": 9
+    },
+    {
+      "date": "04/02/0201",
+      "user": "Bob",
+      "colour": "orange",
+      "make": "Mercedes", 
+      "quantity": 14
+    },
+    {
+      "date": "24/08/2021",
+      "user": "Bob",
+      "colour": "red",
+      "make": "BMW", 
+      "quantity": 23
+    },
+    {
+      "date": "24/12/2021",
+      "user": "Dan",
+      "colour": "red",
+      "make": "BMW", 
+      "quantity": 21
+    },
+    {
+      "date": "30/12/2021",
+      "user": "Alice",
+      "colour": "green",
+      "make": "BMW", 
+      "quantity": 20
+    },
+    {
+      "date": "16/03/2021",
+      "user": "Dan",
+      "colour": "blue",
+      "make": "BMW", 
+      "quantity": 17
+    },
+    {
+      "date": "17/04/2021",
+      "user": "Bob",
+      "colour": "red",
+      "make": "BMW", 
+      "quantity": 20
+    },
+    {
+      "date": "26/10/2021",
+      "user": "Alice",
+      "colour": "green",
+      "make": "BMW", 
+      "quantity": 28
+    },
+    {
+      "date": "11/10/2021",
+      "user": "Alice",
+      "colour": "green",
+      "make": "BMW", 
+      "quantity": 11
+    },
+    {
+      "date": "06/11/2021",
+      "user": "Alice",
+      "colour": "red",
+      "make": "BMW", 
+      "quantity": 10
+    },
+    {
+      "date": "10/11/2021",
+      "user": "Carol",
+      "colour": "green",
+      "make": "BMW", 
+      "quantity": 9
+    },
+    {
+      "date": "03/01/2021",
+      "user": "Alice",
+      "colour": "red",
+      "make": "BMW", 
+      "quantity": 10
+    },
+    {
+      "date": "12/01/2021",
+      "user": "Carol",
+      "colour": "orange",
+      "make": "BMW", 
+      "quantity": 12
+    },
+    {
+      "date": "03/11/2021",
+      "user": "Dan",
+      "colour": "green",
+      "make": "Toyota", 
+      "quantity": 14
+    },
+    {
+      "date": "17/08/2021",
+      "user": "Alice",
+      "colour": "green",
+      "make": "Toyota", 
+      "quantity": 15
+    },
+    {
+      "date": "21/01/2021",
+      "user": "Bob",
+      "colour": "red",
+      "make": "Toyota", 
+      "quantity": 16
+    },
+    {
+      "date": "22/03/2021",
+      "user": "Carol",
+      "colour": "green",
+      "make": "Toyota", 
+      "quantity": 20
+    },
+    {
+      "date": "06/05/2021",
+      "user": "Dan",
+      "colour": "orange",
+      "make": "Toyota", 
+      "quantity": 1
+    },
+    {
+      "date": "07/04/2021",
+      "user": "Carol",
+      "colour": "orange",
+      "make": "Toyota", 
+      "quantity": 20
+    },
+    {
+      "date": "09/09/2021",
+      "user": "Bob",
+      "colour": "red",
+      "make": "Toyota", 
+      "quantity": 9
+    },
+    {
+      "date": "05/02/0201",
+      "user": "Carol",
+      "colour": "blue",
+      "make": "Toyota", 
+      "quantity": 14
+    },
+    {
+      "date": "25/08/2021",
+      "user": "Carol",
+      "colour": "red",
+      "make": "Toyota", 
+      "quantity": 23
+    },
+    {
+      "date": "25/12/2021",
+      "user": "Alice",
+      "colour": "blue",
+      "make": "Toyota", 
+      "quantity": 21
+    },
+    {
+      "date": "31/12/2021",
+      "user": "Dan",
+      "colour": "red",
+      "make": "Toyota", 
+      "quantity": 20
+    },
+    {
+      "date": "17/03/2021",
+      "user": "Alice",
+      "colour": "green",
+      "make": "Ford", 
+      "quantity": 17
+    },
+    {
+      "date": "18/04/2021",
+      "user": "Dan",
+      "colour": "red",
+      "make": "Ford", 
+      "quantity": 20
+    },
+    {
+      "date": "27/10/2021",
+      "user": "Alice",
+      "colour": "red",
+      "make": "Ford", 
+      "quantity": 28
+    },
+    {
+      "date": "12/10/2021",
+      "user": "Dan",
+      "colour": "red",
+      "make": "Ford", 
+      "quantity": 11
+    },
+    {
+      "date": "07/11/2021",
+      "user": "Fred",
+      "colour": "purple",
+      "make": "Ford", 
+      "quantity": 100
+    },
+    {
+      "date": "11/11/2021",
+      "user": "Bob",
+      "colour": "red",
+      "make": "Ford", 
+      "quantity": 9
+    }
+  ];
